b4761c1086fbfd58225ccb7c2fdd98679802b9bf
Shem Irekpita <Shem.irekpita@stu.ocsb.ca>
2025-05-02 14:32:43 +0000

    Update

--- js/script.js
+++ js/script.js
@@ -3,3 +3,18 @@
 // Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
+"use strict";
+function calculate() {
+        let Celsius = parseFloat(document.getElementById("Celsius").value);
+        let Conversion = (Celsius * 9/5) + 32 ;
+        let ans= "";
+    if (Conversion < 15) {
+        ans = "It is cold"
+    }
+    if (Conversion >= 15) {
+        ans = "It is HOT"; 
+    }
+        // Display the results
+    document.getElementById("Conversion").innerHTML = "Conversion " + Conversion.toFixed(1) + "°F";
+        document.getElementById("ans").innerHTML = ans ;
+}
